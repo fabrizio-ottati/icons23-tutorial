d94a0da5075dbdcef5b16992ec04d452e1af20df
fabrizio-ottati <fabriziottati@gmail.com>
2023-07-31 10:56:53 +0200

    Separated on multiple lines the files.

--- test/hls_sim.tcl
+++ test/hls_sim.tcl
@@ -1,9 +1,10 @@
 set FPGA_DEVICE $::env(FPGA_DEVICE)
 
 open_project prj -reset
-add_files -cflags "-I../cc/include" \
-  "dut.cc ../cc/src/lif.cc"
-add_files -cflags "-I../cc/include" -tb "main.cc ../cc/src/utils.cc"
+add_files -cflags "-I../cc/include" "dut.cc"
+add_files -cflags "-I../cc/include" "../cc/src/lif.cc"
+add_files -cflags "-I../cc/include" "../cc/src/utils.cc"
+add_files -cflags "-I../cc/include" -tb "main.cc" 
 open_solution solution1 -reset 
 set_part $FPGA_DEVICE
 set_top dut
