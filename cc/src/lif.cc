8a8ce5de629bc813ce8c7a2bf56e61738a09c018
fabrizio-ottati <fabriziottati@gmail.com>
2023-07-31 11:04:40 +0200

    Added some comments.

--- cc/src/lif.cc
+++ cc/src/lif.cc
@@ -9,6 +9,7 @@
 
 namespace hls4nm {
 
+// Constructor that loads the weights and initializes the membranes.
 LIF_FC::LIF_FC(const weight_t weights[M][N], leak_t leak, thres_t thres)
     : _leak(leak), _thres(thres) {
   reset();
@@ -19,24 +20,28 @@ LIF_FC::LIF_FC(const weight_t weights[M][N], leak_t leak, thres_t thres)
   }
 }
 
+// State initialization.
 void LIF_FC::reset(void) {
   for (auto n = 0; n < N; n++) {
     _states[n] = 0;
   }
 }
 
+// Method that run the layer on an input spikes vector.
 void LIF_FC::run(hls::stream<std::array<spike_t, M>>& dinStream,
                  hls::stream<std::array<spike_t, N>>& doutStream) {
   // Reading the input spikes.
   auto in_spk = dinStream.read();
 
+  // Synaptic currents.
   int currents[N];
 
-  // Resetting the accumulator.
+  // Currents initialization.
   for (auto n = 0; n < N; n++) {
     currents[n] = 0;
   }
 
+  // Reading the input spikes and accumulating synaptic currents.
   for (auto m = 0; m < M; m++) {
     if (in_spk[m]) {
       for (auto n = 0; n < N; n++) {
@@ -47,7 +52,7 @@ void LIF_FC::run(hls::stream<std::array<spike_t, M>>& dinStream,
 
   std::array<spike_t, N> out_spk;
 
-  // Updating the membranes.
+  // Updating the membranes and spiking out.
   for (auto n = 0; n < N; n++) {
     int next_state = _states[n] * _leak;
     next_state = quantize(next_state);
