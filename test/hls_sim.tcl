377a8494dfbfd26c27b6670ae5d2ec86c3dbaac2
fabrizio-ottati <fabriziottati@gmail.com>
2023-08-01 15:26:09 +0200

    Adapted to templates.

--- test/hls_sim.tcl
+++ test/hls_sim.tcl
@@ -2,8 +2,6 @@ set FPGA_DEVICE $::env(FPGA_DEVICE)
 
 open_project prj -reset
 add_files -cflags "-I../cc/include" "dut.cc"
-add_files -cflags "-I../cc/include" "../cc/src/lif.cc"
-add_files -cflags "-I../cc/include" "../cc/src/utils.cc"
 add_files -cflags "-I../cc/include" -tb "main.cc" 
 open_solution solution1 -reset 
 set_part $FPGA_DEVICE
--- test/hls_syn.tcl
+++ test/hls_syn.tcl
@@ -4,8 +4,6 @@ set FPGA_DEVICE $::env(FPGA_DEVICE)
 open_project prj -reset
 open_solution -reset solution1 
 add_files -cflags "-I../cc/include" "dut.cc"
-add_files -cflags "-I../cc/include" "../cc/src/lif.cc"
-add_files -cflags "-I../cc/include" "../cc/src/utils.cc"
 add_files -cflags "-I../cc/include" -tb "main.cc" 
 set_part $FPGA_DEVICE
 create_clock -period "200MHz" -name default
